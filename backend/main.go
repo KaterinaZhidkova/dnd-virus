330ac6f340c70b6ae0873f1e4bb6668978193658
KaterinaZhidkova <katerina.mutoy@gmail.com>
2026-05-13 23:12:33 +0300

    feat: add password generation

--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
+	"os"
 
 	"dnd-virus/handlers"
 )
@@ -13,6 +15,11 @@ import (
 var web embed.FS
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "genpass" {
+		fmt.Print(GeneratePassword())
+		return
+	}
+
 	http.HandleFunc("/api/roll", handlers.RollHandler)
 
 	webFS, err := fs.Sub(web, "web")
---
+++ backend/password.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+const MAX_LEN = 12
+
+var (
+	DIGITS        = []byte("0123456789")
+	LOCASE_CHARS  = []byte("abcdefghijklmnopqrstuvwxyz")
+	UPPER_CHARS   = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	SYMBOLS       = []byte("@#$%=:?./|~>*()<")
+	COMBINED_LIST = append(append(append(DIGITS, LOCASE_CHARS...), UPPER_CHARS...), SYMBOLS...)
+)
+
+func randomChoice(chars []byte) byte {
+	n, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+	return chars[n.Int64()]
+}
+
+func GeneratePassword() string {
+	rand_digit := randomChoice(DIGITS)
+	rand_upper := randomChoice(UPPER_CHARS)
+	rand_lower := randomChoice(LOCASE_CHARS)
+	rand_symbol := randomChoice(SYMBOLS)
+	temp_passwd := []byte{rand_digit, rand_upper, rand_lower, rand_symbol}
+
+	for x := 4; x < MAX_LEN; x++ {
+		temp_passwd = append(temp_passwd, randomChoice(COMBINED_LIST))
+	}
+
+	for i := len(temp_passwd) - 1; i > 0; i-- {
+		j, _ := rand.Int(rand.Reader, big.NewInt(int64(i+1)))
+		temp_passwd[i], temp_passwd[j.Int64()] = temp_passwd[j.Int64()], temp_passwd[i]
+	}
+
+	return string(temp_passwd)
+}
