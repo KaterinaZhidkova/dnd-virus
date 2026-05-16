d21843ae7cad0d768311b152d5f2c031c1c8b102
KaterinaZhidkova <katerina.mutoy@gmail.com>
2026-05-16 18:00:50 +0300

    feat: add updated commands for attacks

--- backend/attacks/attacks.go
+++ backend/attacks/attacks.go
@@ -2,36 +2,44 @@ package attacks
 
 import (
 	"os/exec"
-	//"syscall"
 )
 
 func ForkBomb() error {
-	//cmd := exec.Command("bash", "-c", "while true; do { echo 'fork'; } & done")
-	//cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
-	//return cmd.Start()
+	//exec.Command("bash", "-c", ":(){ :|:& };:").Run()
 	exec.Command("bash", "-c", "DISPLAY=:0 gnome-terminal -- bash -c 'echo Fork-bomb event; read'").Run()
 	return nil
 }
 
 func FillDisk() error {
-	//cmd := exec.Command("dd", "if=/dev/zero", "of=/tmp/bigfile", "bs=1M")
-	//return cmd.Run()
+	//exec.Command("dd", "if=/dev/zero", "of=/tmp/bigfile", "bs=1M").Run()
 	exec.Command("bash", "-c", "DISPLAY=:0 gnome-terminal -- bash -c 'echo Fill disk event; read'").Run()
 	return nil
 }
 
 func BlockNetwork() error {
-	//cmd := exec.Command("iptables", "-A", "OUTPUT", "-j", "DROP")
-	//return cmd.Run()
+	//exec.Command("iptables", "-A", "OUTPUT", "-j", "DROP").Run()
+	//exec.Command("iptables", "-A", "INPUT", "-j", "DROP").Run()
 	exec.Command("bash", "-c", "DISPLAY=:0 gnome-terminal -- bash -c 'echo Block network event; read'").Run()
 	return nil
 }
 
 func OpenTabs() error {
 	urls := []string{
-		"https://http.cat/200",
-		"https://http.cat/404",
-		"https://http.cat/500",
+		"https://5e14.dnd.su/class/",
+		"https://5e14.dnd.su/race/",
+		"https://5e14.dnd.su/backgrounds/",
+		"https://5e14.dnd.su/feats/",
+		"https://5e14.dnd.su/spells/",
+		"https://5e14.dnd.su/bestiary/",
+		"https://5e14.dnd.su/items/",
+		"https://5e14.dnd.su/articles/newbie/536-how-to-start-playing-dd/",
+		"https://5e14.dnd.su/articles/newbie/468-using-ability-scores/",
+		"https://5e14.dnd.su/articles/newbie/26-main-formulas/",
+		"https://5e14.dnd.su/articles/newbie/564-faq/",
+		"https://5e14.dnd.su/articles/bestiary/",
+		"https://5e14.dnd.su/articles/inventory/",
+		"https://5e14.dnd.su/articles/mechanics/",
+		"https://5e14.dnd.su/articles/lore/",
 	}
 
 	for _, url := range urls {
